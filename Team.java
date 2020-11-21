0104849f14f79c5b2b21efe104888a168b0786be
Yaksh Haranwala <yjharanwala@mun.ca>
2020-11-20 23:27:26 -0500

    Added blueprint for team and Division class

--- Team.java
+++ Team.java
@@ -1,32 +1,38 @@
 /**
- * Write a description of class Team here.
+ * The Team class is the actual representation of each team that 
+ * is going to participate in the in the Division as well as is 
+ * going to play matches.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Salman Haidri, Yaksh J Haranwala, Hassan Zobaer Chowdhury.
+ * @version 20th November 2020
  */
 public class Team
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    // The Name of the Team
+    private String teamName;
+    // The Division that the team is playing in.
+    private Division division;
+    // The number of matches that the Team has played.
+    private int matchesPlayed;
+    // The number of matches that the Team has Won.
+    private int matchesWon;
+    // The number of matches that the Team has Lost.
+    private int matchesLost;
+    // The number of points scored by the Team in the Division.
+    private int pointsFor;
+    // The number of points scored against the Team in the Division.
+    private int pointsAgainst;   
+    // Point difference PF - PA.
+    private int pointsDifference;
 
     /**
-     * Constructor for objects of class Team
+     * Constructor for objects of class Team which
+     * assigns all the necessary attributes to it.
      */
-    public Team()
+    public Team(String teamName, Division division)
     {
         // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        this.teamName = teamName;
+        this.division = division;
     }
 }
